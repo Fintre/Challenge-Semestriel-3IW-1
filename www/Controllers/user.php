2dcce4333d7f1f18cb408a9590dfdf3e63f7df49
Catalina DANILA <catalinadanila6@gmail.com>
2023-11-27 19:54:52 +0100

    ajout des commentaires

--- www/Controllers/user.php
+++ www/Controllers/user.php
@@ -1,35 +1,143 @@
 <?php
+namespace App\Models;
+use App\Core\DB;
 
-namespace App\Controllers;
-
-use App\Core\View;
-
-class User
+class User extends DB
 {
+    private ?int $id = null;
+    protected string $firstname;
+    protected string $lastname;
+    protected string $email;
+    protected string $pwd;
+    protected int $status;
+    protected int $isDeleted;
 
-    public function allUsers(): void
+
+    public function __construct()
     {
-        echo "tous les utilisateurs";
+        parent::__construct();
     }
 
-    public function newUser(): void
+    public function __toString()
     {
-        $newUser = new View("User/newuser", "front");
+        return $this->getFirstname()." ".$this->getLastname();
     }
 
-    public function edit(): void
+    /**
+     * @return int
+     */
+    public function getId(): ?int
     {
-        $editUser = new View("User/edituser", "front");
+        return $this->id;
     }
 
-
-    public function delete(): void
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
     {
-        echo "Supprimmer un utilisateur";
+        $this->id = $id;
     }
 
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
 
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname(string $firstname): void
+    {
+        $firstname = ucwords(strtolower(trim($firstname)));
+        $this->firstname = $firstname;
+    }
 
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname(string $lastname): void
+    {
+        $lastname = strtoupper(trim($lastname));
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $email = strtolower(trim($email));
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPwd(): string
+    {
+        return $this->pwd;
+    }
+
+    /**
+     * @param string $pwd
+     */
+    public function setPwd(string $pwd): void
+    {
+        $pwd = password_hash($pwd, PASSWORD_DEFAULT);
+        $this->pwd = $pwd;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param int $status
+     */
+    public function setStatus(int $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDeleted(): bool
+    {
+        return $this->isDeleted;
+    }
+
+    /**
+     * @param bool $isDeleted
+     */
+    public function setIsDeleted(bool $isDeleted): void
+    {
+        $this->isDeleted = $isDeleted;
+    }
 
 
 }
