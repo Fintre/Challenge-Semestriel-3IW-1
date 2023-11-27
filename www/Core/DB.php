b68764f50c5c8f3dd3537a567e35bc6901001056
Catalina DANILA <catalinadanila6@gmail.com>
2023-11-27 20:24:37 +0100

    ajout fichier db.php

---
+++ www/Core/DB.php
@@ -0,0 +1,89 @@
+<?php
+namespace App\Core;
+
+class DB
+{
+    private ?object $pdo = null;
+    private string $table;
+
+    public function __construct()
+    {
+        //connexion à la bdd via pdo
+        try{
+            $this->pdo = new \PDO("pgsql:host=postgres;dbname=gfm;charset=utf8", "gofindme", "gfmpwd");
+        }catch (\PDOException $e) {
+            echo "Erreur SQL : ".$e->getMessage();
+        }
+
+        $table = get_called_class(); //pour récupérer le nom de la classe qui a appelé la méthode
+        $table = explode("\\", $table); //pour séparer le namespace du nom de la classe
+        $table = array_pop($table); //pour récupérer le nom de la classe
+        $this->table = "gfm_".strtolower($table); //pour mettre le nom de la table en minuscule et ajouter le préfixe gfm_
+    }
+
+    public function getDataObject(): array //pour récupérer les données de l'objet
+    {
+        return array_diff_key(get_object_vars($this), get_class_vars(get_class())); //mettre dans un tableau les données de l'objet
+    }
+
+    public function save() //pour insérer ou mettre à jour les données de l'objet dans la bdd
+    {
+        $data = $this->getDataObject();
+
+        if( empty($this->getId())){ //si l'id est vide, on insère
+            $sql = "INSERT INTO " . $this->table . "(" . implode(",", array_keys($data)) . ")
+            VALUES (:" . implode(",:", array_keys($data)) . ")";
+        }else{ //sinon on met à jour
+            $sql = "UPDATE " . $this->table . " SET ";
+            foreach ($data as $column => $value){
+                $sql.= $column. "=:".$column. ",";
+            }
+            $sql = substr($sql, 0, -1);
+            $sql.= " WHERE id = ".$this->getId();
+        }
+
+
+        $queryPrepared = $this->pdo->prepare($sql); //pour préparer la requête
+        $queryPrepared->execute($data); //pour exécuter la requête
+    }
+
+
+    public static function populate(int $id): object
+    {
+        $class = get_called_class();
+        $object = new $class();
+        return $object->getOneBy(["id"=>$id], "object");
+    }
+
+    //$data = ["id"=>1] ou ["email"=>"y.skrzypczyk@gmail.com"]
+    public function getOneBy(array $data, string $return = "array") //pour récupérer un enregistrement de la bdd
+    {
+        //SELECT * FROM gfm_user WHERE id=:id AND ...
+        $sql = "SELECT * FROM ".$this->table. " WHERE ";
+        foreach ($data as $column=>$value){
+            $sql .= " ".$column."=:".$column. " AND";
+        }
+        $sql = substr($sql, 0, -3); //pour enlever le dernier AND
+        $queryPrepared = $this->pdo->prepare($sql); //pour préparer la requête
+        $queryPrepared->execute($data); //pour exécuter la requête
+        if($return == "object"){//pour récupérer un objet
+            $queryPrepared->setFetchMode(\PDO::FETCH_CLASS, get_called_class()); //les resultats seront sous forme d'objet de la classe appelée
+        }
+
+        return $queryPrepared->fetch(); //pour récupérer le résultat de la requête (un seul enregistrement)
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
