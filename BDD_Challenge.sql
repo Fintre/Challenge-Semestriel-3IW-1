d7ca40fe348d83bf848143be32a7338bccf7f76c
Kevin BATISTA DOS SANTOS <batistakevin041@gmail.com>
2023-12-12 16:44:38 +0100

    Ajout img_path pour user et email unique

--- BDD_Challenge.sql
+++ BDD_Challenge.sql
@@ -4,67 +4,38 @@ SET search_path = public;
 --
 
 -- Structure de la table `categorie`
-<<<<<<< HEAD
-CREATE TABLE "categorie" (
-  "id" serial NOT NULL,
-  "description" varchar(255) NOT NULL,
-  "createdAt" timestamp DEFAULT current_timestamp() NOT NULL,
-  "updatedAt" timestamp DEFAULT current_timestamp() ON UPDATE current_timestamp() NOT NULL,
-=======
 CREATE TABLE "gfm_categorie" (
   "id" serial NOT NULL,
   "description" varchar(255) NOT NULL,
   "createdat" timestamp DEFAULT current_timestamp NOT NULL,
   "updatedat" timestamp DEFAULT current_timestamp,
->>>>>>> main
   "isDeleted" smallint NOT NULL DEFAULT 0
 );
 
 -- Structure de la table `comment`
-<<<<<<< HEAD
-CREATE TABLE "comment" (
-  "id" serial NOT NULL,
-  "commentText" varchar(255) NOT NULL,
-  "createdAt" timestamp DEFAULT current_timestamp() NOT NULL,
-  "updatedAt" timestamp DEFAULT current_timestamp() ON UPDATE current_timestamp() NOT NULL,
-=======
 CREATE TABLE "gfm_comment" (
   "id" serial NOT NULL,
   "commentText" varchar(255) NOT NULL,
   "createdat" timestamp DEFAULT current_timestamp NOT NULL,
   "updatedat" timestamp DEFAULT current_timestamp,
->>>>>>> main
   "user_id" int NOT NULL,
   "post_id" int NOT NULL
 );
 
 -- Structure de la table `media`
-<<<<<<< HEAD
-CREATE TABLE "media" (
-=======
 CREATE TABLE "gfm_media" (
->>>>>>> main
   "id" serial NOT NULL,
   "tag" varchar(15) NOT NULL,
   "filepath" varchar(100) NOT NULL,
   "description" varchar(255) NOT NULL,
-<<<<<<< HEAD
-  "createdAt" timestamp NOT NULL,
-  "updatedAt" timestamp NOT NULL,
-=======
   "createdat" timestamp DEFAULT current_timestamp NOT NULL,
   "updatedat" timestamp DEFAULT current_timestamp,
->>>>>>> main
   "isDeleted" smallint NOT NULL DEFAULT 0,
   "post_id" int NOT NULL
 );
 
 -- Structure de la table `post`
-<<<<<<< HEAD
-CREATE TABLE "post" (
-=======
 CREATE TABLE "gfm_post" (
->>>>>>> main
   "id" serial NOT NULL,
   "title" varchar(40) NOT NULL,
   "body" text NOT NULL,
@@ -73,88 +44,40 @@ CREATE TABLE "gfm_post" (
   "slug" varchar(20) NOT NULL,
   "published" smallint NOT NULL DEFAULT 0,
   "isDeleted" smallint NOT NULL DEFAULT 0,
-<<<<<<< HEAD
-  "createdAt" timestamp DEFAULT current_timestamp() NOT NULL,
-  "updatedAt" timestamp DEFAULT current_timestamp() ON UPDATE current_timestamp() NOT NULL,
-=======
   "createdat" timestamp DEFAULT current_timestamp NOT NULL,
   "updatedat" timestamp DEFAULT current_timestamp,
->>>>>>> main
   "user_id" int NOT NULL,
   "siteSetting_id" int NOT NULL
 );
 
 -- Structure de la table `siteSetting`
-<<<<<<< HEAD
-CREATE TABLE "siteSetting" (
-  "id" serial NOT NULL,
-  "clé" int NOT NULL,
-  "valeur" varchar(255) NOT NULL,
-  "createdAt" timestamp DEFAULT current_timestamp() NOT NULL,
-  "updatedAt" timestamp DEFAULT current_timestamp() ON UPDATE current_timestamp() NOT NULL,
-=======
 CREATE TABLE "gfm_siteSetting" (
   "id" serial NOT NULL,
   "clé" int NOT NULL,
   "valeur" varchar(255) NOT NULL,
   "createdat" timestamp DEFAULT current_timestamp NOT NULL,
   "updatedat" timestamp DEFAULT current_timestamp,
->>>>>>> main
   "isDeleted" smallint NOT NULL DEFAULT 0
 );
 
 -- Structure de la table `user`
-<<<<<<< HEAD
-CREATE TABLE "user" (
-=======
 CREATE TABLE "gfm_user" (
->>>>>>> main
   "id" serial NOT NULL,
   "firstname" varchar(25) NOT NULL,
   "lastname" varchar(50) NOT NULL,
-  "email" varchar(320) NOT NULL,
+  "email" varchar(320) NOT NULL UNIQUE,
   "username" varchar(25) NOT NULL,
-<<<<<<< HEAD
-  "password" varchar(255) NOT NULL,
-  "status" smallint NOT NULL DEFAULT 0,
-  "roles" varchar(15) NOT NULL,
-  "createdAt" timestamp DEFAULT current_timestamp() NOT NULL,
-  "updatedAt" timestamp DEFAULT current_timestamp() ON UPDATE current_timestamp() NOT NULL
-=======
   "pwd" varchar(255) NOT NULL,
   "status" smallint NOT NULL DEFAULT 0,
+  "img_path" varchar(255),
   "roles" varchar(15) NOT NULL,
   "createdat" timestamp DEFAULT current_timestamp NOT NULL,
   "updatedat" timestamp DEFAULT current_timestamp
->>>>>>> main
 );
 
 -- Index pour les tables déchargées
 
 -- Index pour la table `categorie`
-<<<<<<< HEAD
-CREATE UNIQUE INDEX "categorie_pkey" ON "categorie" ("id");
-
--- Index pour la table `comment`
-CREATE UNIQUE INDEX "comment_pkey" ON "comment" ("id");
-CREATE INDEX "comment_user_id" ON "comment" ("user_id");
-CREATE INDEX "comment_post_id" ON "comment" ("post_id");
-
--- Index pour la table `media`
-CREATE UNIQUE INDEX "media_pkey" ON "media" ("id");
-CREATE INDEX "media_post_id" ON "media" ("post_id");
-
--- Index pour la table `post`
-CREATE UNIQUE INDEX "post_pkey" ON "post" ("id");
-CREATE INDEX "post_user_id" ON "post" ("user_id");
-CREATE INDEX "post_siteSetting_id" ON "post" ("siteSetting_id");
-
--- Index pour la table `siteSetting`
-CREATE UNIQUE INDEX "siteSetting_pkey" ON "siteSetting" ("id");
-
--- Index pour la table `user`
-CREATE UNIQUE INDEX "user_pkey" ON "user" ("id");
-=======
 CREATE UNIQUE INDEX "gfm_categorie_pkey" ON "gfm_categorie" ("id");
 
 -- Index pour la table `comment`
@@ -176,25 +99,10 @@ CREATE UNIQUE INDEX "gfm_siteSetting_pkey" ON "gfm_siteSetting" ("id");
 
 -- Index pour la table `user`
 CREATE UNIQUE INDEX "gfm_user_pkey" ON "gfm_user" ("id");
->>>>>>> main
 
 -- Contraintes pour les tables déchargées
 
 -- Contraintes pour la table `comment`
-<<<<<<< HEAD
-ALTER TABLE "comment"
-  ADD CONSTRAINT "comment_user_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT "comment_post_fk" FOREIGN KEY ("post_id") REFERENCES "post" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-
--- Contraintes pour la table `media`
-ALTER TABLE "media"
-  ADD CONSTRAINT "media_post_fk" FOREIGN KEY ("post_id") REFERENCES "post" ("id") ON DELETE NO ACTION ON UPDATE CASCADE;
-
--- Contraintes pour la table `post`
-ALTER TABLE "post"
-  ADD CONSTRAINT "post_user_fk" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT "post_siteSetting_fk" FOREIGN KEY ("siteSetting_id") REFERENCES "siteSetting" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
-=======
 ALTER TABLE "gfm_comment"
   ADD CONSTRAINT "gfm_comment_user_fk" FOREIGN KEY ("user_id") REFERENCES "gfm_user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT "gfm_comment_post_fk" FOREIGN KEY ("post_id") REFERENCES "gfm_post" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
@@ -207,4 +115,3 @@ ALTER TABLE "gfm_media"
 ALTER TABLE "gfm_post"
   ADD CONSTRAINT "gfm_post_user_fk" FOREIGN KEY ("user_id") REFERENCES "gfm_user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT "gfm_post_siteSetting_fk" FOREIGN KEY ("siteSetting_id") REFERENCES "gfm_siteSetting" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
->>>>>>> main
