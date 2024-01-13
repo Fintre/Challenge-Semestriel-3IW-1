9c6d581f6b53e8ead6c6ff4097d800de9a3a04d4
cgirard <camille.girard1995@gmail.com>
2024-01-13 20:20:36 +0100

    :construction: wip

--- www/Views/components/Post/post.view.php
+++ www/Views/components/Post/post.view.php
@@ -8,7 +8,23 @@
     <title>Mes pages</title>
 </head>
 <body>
-<h2>Mes pages</h2>
-<!-- contenu de ma page ici !-->
+    <header id="header" class="post-header">
+        <h2>Mes pages</h2>
+    </header>
+    <main>
+        <section class="section1-status-tab">
+            <div class="status-tab">
+                <ul id="tab">
+                    <li><a href="#">Tous</a></li>
+                    <li><a href="#">Publiées</a></li>
+                    <li><a href="#">Brouillon</a></li>
+                    <li><a href="#">Corbeille</a></li>
+                </ul>
+            </div>
+        </section>
+    </main>
+
+
+    <!-- contenu de ma page ici !-->
 </body>
 </html>
