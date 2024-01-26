133304d8527820289379ce41695bf00401008cdb
cgirard <camille.girard1995@gmail.com>
2024-01-26 23:16:21 +0100

    :sparkles: mise en page et ajout de style sur new post

--- www/Views/components/Post/newPost.view.php
+++ www/Views/components/Post/newPost.view.php
@@ -1,65 +1,78 @@
 <h2>Nouvelle page</h2>
-<section class="section1-new-post-container">
-    <form>
-        <div class="form-group">
-            <label for="pageName"></label>
-            <textarea type="text" id="pageName" class="page-name" placeholder="Nom de la page ..."></textarea>
+<section class="new-post-main">
+    <div class="section1-new-post-container">
+        <form>
+            <div class="form-group">
+                <label for="pageName"></label>
+                <textarea type="text" id="pageName" class="page-name" placeholder="Nom de la page ..."></textarea>
+            </div>
+            <div class="form-group">
+                <label for="pageTitle"></label>
+                <textarea type="text" id="pageName" class="page-title" placeholder="Titre de la page ..."></textarea>
+            </div>
+            <div class="form-group">
+                <label for="pageContent"></label>
+                <textarea type="text" id="pageName" class="page-content" placeholder="Saisir votre texte ..."></textarea>
+            </div>
+            <div class="form-button-add-element">
+                <button class="button button-primary button-sm">+</button>
+            </div>
+
+        </form>
+        <div class="form-footer">
+            <button class="button button-primary button-lg">Ajouter</button>
         </div>
-        <div class="form-group">
-            <label for="pageTitle"></label>
-            <textarea type="text" id="pageName" class="page-title" placeholder="Titre de la page ..."></textarea>
-        </div>
-        <div class="form-group">
-            <label for="pageContent"></label>
-            <textarea type="text" id="pageName" class="page-content" placeholder="Saisir votre texte ..."></textarea>
-        </div>
-        <div class="form-actions">
-            <div class="form-actions-wrapper">
-            <button class="button button-primary button-sm">+</button>
+    </div>
+
+
+
+
+    <div>
+        <div class="block-card block-card-custom-page color">Couleur
+            <div class="block-card block-card-custom-page-color-wrapper">
+                <span class="block-card-custom-page-color-content">Texte
+                    <input type="color" id="color-text" name="color-text"/><label for="color-text"></label>
+                </span>
+                <span class="block-card-custom-page-color-content">A-Plan
+                    <input type="color" id="color-text-background" name="color-text-background"/><label for="color-text-background"></label>
+                </span>
             </div>
         </div>
-    </form>
-</section>
-<section class="section2-page-add">
-    <button class="button button-primary">Ajouter</button>
-</section>
-<section>
-    <div class="block-card block-card-custom-page color">Couleur
-        <div class="block-card-custom-page-color-wrapper">
-					<span class="block-card-custom-page-text">Texte
-						<input type="color" id="color-text" name="color-text"/><label for="color-text"></label>
-					</span>
-            <span class="block-card-custom-page-background">A-Plan
-						<input type="color" id="color-text-background" name="color-text-background"/><label for="color-text-background"></label>
-					</span>
-        </div>
-    </div>
-    <div class="block-card block-card-custom-page info">
-        <div class="block-card-custom-page-info-property">Auteur<span class="block-card-custom-page-info-value">Yves</span></div>
-        <div class="block-card-custom-page-info-property">Statut<span class="block-card-custom-page-info-value">Brouillon</span></div>
-        <div class="block-card-custom-page-info-property">Date<span class="block-card-custom-page-info-value">12/12/2023</span></div>
-        <div class="block-card-custom-page-info-property">URL<span class="block-card-custom-page-info-value">gofindme.com/home</span></div>
-    </div>
-    <div class="block-card block-card-custom-page media">
-        <div class="block-card-custom-page-media-property">Image<span class="block-card-custom-page-media-value">Ajouter un nouveau contenu</span></div>
-    </div>
-    <div class="block-card block-card-custom-page typo">
-        <div class="block-card-custom-page-typo-property">Typographie
-            <span class="block-card-custom-page-typo-value">lato</span>
-            <div class="block-card-custom-page-typo-wrapper">
-                <div class="block-card-custom-page-typo-property">Taille de police
-                    <span class="block-card-custom-page-typo-value">24 px</span>
-                    <span class="block-card-custom-page-typo-value">-</span>
-                    <span class="block-card-custom-page-typo-value">+</span>
-                    <span class="block-card-custom-page-typo-value">h1</span>
-                    <span class="block-card-custom-page-typo-value">h2</span>
-                    <span class="block-card-custom-page-typo-value">h3</span>
-                    <span class="block-card-custom-page-typo-value">p</span>
-                    <span class="block-card-custom-page-typo-value">liste</span>
-                    <span class="block-card-custom-page-typo-value">lien</span>
-                    <span class="block-card-custom-page-typo-value">a</span>
-                    <span class="block-card-custom-page-typo-value">A</span>
+        <div class="block-card block-card-custom-page typo">
+            <div class="block-card-custom-page-typo-property">Typographie
+                <span class="block-card-custom-page-typo-value">lato</span>
+                <div class="block-card-custom-page-typo-wrapper">
+                    <div class="block-card-custom-page-typo-property">Taille de police
+                        <button class="block-card-custom-page-typo-value">24 px</button>
+                        <button class="block-card-custom-page-typo-value">-</button>
+                        <button class="block-card-custom-page-typo-value">+</button>
+                        <button class="block-card-custom-page-typo-value">h1</button>
+                        <button class="block-card-custom-page-typo-value">h2</button>
+                        <button class="block-card-custom-page-typo-value">h3</button>
+                        <button class="block-card-custom-page-typo-value">p</button>
+                        <button class="block-card-custom-page-typo-value">liste</button>
+                        <button class="block-card-custom-page-typo-value">lien</button>
+                        <button class="block-card-custom-page-typo-value">a</button>
+                        <button class="block-card-custom-page-typo-value">A</>
+                    </div>
                 </div>
+            </div>
+        </div>
+        <div class="block-card block-card-custom-page info">
+            <div class="block-card-custom-page-info-property">Auteur<span class="block-card-custom-page-info-value">Yves</span></div>
+            <div class="block-card-custom-page-info-property">Statut<span class="block-card-custom-page-info-value">Brouillon</span></div>
+            <div class="block-card-custom-page-info-property">Date<span class="block-card-custom-page-info-value">12/12/2023</span></div>
+            <div class="block-card-custom-page-info-property">URL<span class="block-card-custom-page-info-value">gofindme.com/home</span></div>
+        </div>
+        <div class="block-card block-card-custom-page media">
+            <div class="block-card-custom-page-media-property">Image<span class="block-card-custom-page-media-value">Ajouter un nouveau contenu</span></div>
+        </div>
+        <div class="button-new-page">
+            <div class="button-submit">
+                <button class="button button-primary button-lg">Sauvegarder</button>
+            </div>
+            <div class="button-delet">
+                <button class="button button-danger button-lg">Supprimer</button>
             </div>
         </div>
     </div>
