f2cdbdecac0f09b64705dd423247400182e7959d
Soumaya <gambonanasoumaya@gmail.com>
2024-02-07 16:34:16 +0100

    html setting / tableau des users dynamique (données de la bdd)

--- www/Views/components/Setting/index.view.php
+++ www/Views/components/Setting/index.view.php
@@ -1,6 +1,15 @@
 <h1>Réglages</h1>
 <section class="settings-bloc">
-    
+    <div class="setting-content">
+        <div class="site-image">
+            <img src="https://sf1.lechasseurfrancais.com/wp-content/uploads/2023/11/shutterstock_2321514999-scaled.jpg" alt="image" width="300" height="200" class="blog-image">
+        </div>
+        <div>Formulaire ici avec input type=file (pour ajouter l'image de profil)</div>
+    </div>
+</section>
+<section class="setting-button">
+    <button class="button button-primary">Lancer le site</button>
+    <button class="button button-danger">Supprimer le site le site</button>
 </section>
 
 
