aaf17b6360ca1329598e978076dbcc40c3364f87
Catalina DANILA <catalinadanila6@gmail.com>
2024-02-16 23:24:43 +0100

    update css for forms

--- www/Views/components/Security/login.view.php
+++ www/Views/components/Security/login.view.php
@@ -2,5 +2,7 @@
     <div class="box-title"><h3>Connexion</h3></div>
     <div class="box-content">
         <?php $this->includeComponent("form", $configForm, $errorsForm, $successForm, "button button-primary");?>
+        <p class="text">Pas encore de compte ? <a href="/register">Inscrivez-vous</a></p>
+        <p class="text">Mot de passe oublié ? <a href="/recover-password">Cliquez ici</a></p>
     </div>
 </section>
