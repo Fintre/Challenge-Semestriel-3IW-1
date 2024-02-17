f64e372129fcf961581b122584d6d13567480bef
Soumaya <gambonanasoumaya@gmail.com>
2024-02-17 17:53:04 +0100

    refonte

--- www/Views/components/Articles/allArticles.view.php
+++ www/Views/components/Articles/allArticles.view.php
@@ -1,60 +1,34 @@
 <?php namespace App\Controllers; ?>
-<!-- Inclure jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-<h1>Mes articles</h1>
-<section class="button-section">
-    <div class="filters">
-        <ul class="link-list">
-            <li><a href="#">Publiés </a></li>
-            <li><a href="#">Brouillon</a></li>
-            <li><a href="#">Corbeille</a></li>
-            <li><div class="fix-right"><a href="/articles/add-article"><button class="button button-primary">Ajouter un nouveau</button></a></div></li>
-        </ul>
-    </div>
+<h2>Mes articles</h2>
+<section class="section1-status-tab">
+    <table class="status-tab">
+        <thead>
+            <tr class="tab">
+                <th class="tab-item active"><a href="#">Tous</a></th>
+                <th class="tab-item"><a href="#">Publiées</a></th>
+                <th class="tab-item"><a href="#">Brouillon</a></th>
+                <th class="tab-item"><a href="#">Corbeille</a></th>
+            </tr>
+        </thead>
+    </table>
 </section>
-
 <section class="all-blogs">
-    <div class="my-blogs">
-        <?php 
-            $articleController = new \App\Controllers\Articles();
-            $allArticles = $articleController->getAllArticles();
+    <?php 
+        $articleController = new \App\Controllers\Articles();
+        $allArticles = $articleController->getAllArticles();
 
-            foreach ($allArticles as $articleData): ?>
-            <div class="one-blog">
-                <div class="edit-icon">
-                    <a href="/articles/edit-article?article=<?php echo $articleData['id']; ?>" class="link-primary">Edit</a>
-                    <a href="#" id="deleteArticleLink" data-article-id="<?php echo $articleData['id']; ?>" class="link-danger">Supprimer</a>
-                </div>
-                <div class="blog-title"><h3><?php echo $articleData['title']; ?></h3></div>
-                <div class="article-text"><?php echo $articleData['description']; ?></div>
-                <div class="blog-date">Pulié le : <?php echo date('Y-m-d', strtotime($articleData['createdat'])); ?></div>
+        foreach ($allArticles as $articleData): ?>
+        <div class="one-blog">
+            <div class="edit-icon">
+                <a href="/articles/edit-article?article=<?php echo $articleData['id']; ?>" class="link-primary">Edit</a>
+                <!-- integrer un formulaire ici avec un button : class="link-danger" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet article ?')<a href="#" id="deleteArticleLink" data-article-id="<?php echo $articleData['id']; ?>" class="link-danger">Supprimer</a> -->
             </div>
-        <?php endforeach; ?>
-    </div>
+            <div class="blog-title"><h4><?php echo $articleData['title']; ?></h4></div>
+            <div class="article-text"><?php echo $articleData['description']; ?></div>
+            <div class="blog-date">Pulié le : <?php echo date('Y-m-d', strtotime($articleData['createdat'])); ?></div>
+        </div>
+    <?php endforeach; ?>
 </section>
-
-<script>
-$(document).ready(function() {
-    $("#deleteArticleLink").on("click", function(event) {
-        // Empêcher le comportement par défaut du lien
-        event.preventDefault();
-        var articleId = $(this).data("article-id");
-        console.log(articleId)
-
-        // Effectuer la requête AJAX
-        $.ajax({
-            url: "../../../Controllers/Articles.php",
-            method: "POST",
-            data: { id: articleId, action: "deleteArticle" },
-            success: function(response) {
-                console.log(response);
-                //location.reload();
-            },
-            error: function(error) {
-                console.error("Erreur AJAX:", error);
-            }
-        });
-    });
-});
-</script>
-
+<section class="section5-page-add">
+    <button class="button button-primary">Ajouter un nouvel article</button>
+</section>
--- www/Views/components/User/allusers.view.php
+++ www/Views/components/User/allusers.view.php
@@ -1,5 +1,5 @@
-<h1>Tous les utilisateurs</h1>
-<section class="users-table">
+<h2>Tous les utilisateurs</h2>
+<section class="section1-status-tab">
 <div class="user-table">
     <table class="responsive-table" id="myTable">
         <thead class="responsive-th">
@@ -10,33 +10,6 @@
                 <th>Action</th>
             </tr>
         </thead>
-        <!-- <tbody class="responsive-tb">
-            <tr>
-                <td>user1</td>
-                <td>user@gmail.com</td>
-                <td id="adminCell" class="adminCell"></td>
-                <td><a href="#" class="link-danger">supprimer</a></td>
-            </tr>
-            <tr>
-                <td>user2</td>
-                <td>user@gmail.com</td>
-                <td>admin</td>
-                <td><a href="#" class="link-danger">supprimer</a></td>
-            </tr>
-            <tr>
-                <td>user3</td>
-                <td>user@gmail.com</td>
-                <td>admin</td>
-                <td><a href="#" class="link-danger">supprimer</a></td>
-            </tr>
-            <tr>
-                <td>user4</td>
-                <td>user@gmail.com</td>
-                <td>admin</td>
-                <td><a href="#" class="link-danger">supprimer</a></td>
-            </tr>
-        </tbody> -->
-
         <tbody class="responsive-tb">
             <?php
 
@@ -60,7 +33,6 @@
     $(document).ready(function() {
         var table = $('#myTable').DataTable({
             "rowCallback": function(row, data, index) {
-                // Vérifiez si l'index de la ligne est pair
                 if (index % 2 === 0) {
                     $(row).css("background-color", "white");
                 } else {
@@ -68,7 +40,6 @@
                 }
             },
             "drawCallback": function(settings) {
-                // Réappliquez la couleur d'arrière-plan après chaque redessin
                 var rows = table.rows({ page: 'current' }).nodes();
                 $(rows).each(function(index) {
                     if (index % 2 === 0) {
@@ -80,24 +51,5 @@
             }
         });
     });
-
-    var adminTd = document.getElementById('adminCell');
-
-    // Créer un élément <select>
-    var selectElement = document.createElement('select');
-
-    // Ajouter des options à la liste déroulante
-    var options = ['Admin', 'Utilisateur', ''];
-
-    for (var i = 0; i < options.length; i++) {
-        var option = document.createElement('option');
-        option.value = options[i];
-        option.text = options[i];
-        selectElement.appendChild(option);
-    }
-
-    // Remplacer le contenu du <td> par la liste déroulante
-    adminTd.innerHTML = '';
-    adminTd.appendChild(selectElement);
 
 </script>
