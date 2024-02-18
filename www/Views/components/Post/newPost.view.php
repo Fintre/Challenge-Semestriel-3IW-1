16b26efec765495ca876c79ec551d0ca3dc77061
cgirard <camille.girard1995@gmail.com>
2024-02-18 19:20:23 +0100

    add dark mode style

--- www/Views/components/Post/newPost.view.php
+++ www/Views/components/Post/newPost.view.php
@@ -42,74 +42,43 @@ if (empty($this->data['mandatoryFields'])) {
                 <textarea name="pageContent"
                           id="pageContent"><?php echo $this->data['post']->getBody() ?? '' ?></textarea>
             </div>
-            <div hidden>
-                <input type="number" name="id" value="<?php echo $this->data['post']->getId() ?? '' ?>"/>
-                <input type="number" name="isDeleted" value="<?php echo $this->data['post']->getIsDeleted() ?? '0' ?>"/>
-                <input type="number" name="isPublished" value="<?php echo $this->data['post']->getPublished() ?? '0' ?>"/>
-            </div>
-
-            <div class="form-button-add-element">
-                <button class="button button-primary button-sm">+</button>
-            </div>
             <div class="form-footer">
-                <button type="submit" class="button button-primary button-lg">Ajouter</button>
+                <button type="submit" class="button button-primary button-lg">Sauvegarder</button>
             </div>
         </form>
     </div>
 
     <div class="section2-new-post-container">
-        <div class="block-card block-card-custom-page color">Couleur
-            <div class="block-card block-card-custom-page-color-wrapper">
-                <span class="block-card-custom-page-color-content">Texte
-                    <input type="color" id="color-text" name="color-text"/><label for="color-text"></label>
-                </span>
-                <span class="block-card-custom-page-color-content">A-Plan
-                    <input type="color" id="color-text-background" name="color-text-background"/><label
-                            for="color-text-background"></label>
-                </span>
-            </div>
-        </div>
-        <div class="block-card block-card-custom-page typo">
-            <div class="block-card-custom-page-typo-property">Typographie
-                <span class="block-card-custom-page-typo-value">lato</span>
-                <div class="block-card-custom-page-typo-wrapper">
-                    <div class="block-card-custom-page-typo-property">Taille de police
-                        <button class="block-card-custom-page-typo-value">24 px</button>
-                        <button class="block-card-custom-page-typo-value">-</button>
-                        <button class="block-card-custom-page-typo-value">+</button>
-                        <button class="block-card-custom-page-typo-value">h1</button>
-                        <button class="block-card-custom-page-typo-value">h2</button>
-                        <button class="block-card-custom-page-typo-value">h3</button>
-                        <button class="block-card-custom-page-typo-value">p</button>
-                        <button class="block-card-custom-page-typo-value">liste</button>
-                        <button class="block-card-custom-page-typo-value">lien</button>
-                        <button class="block-card-custom-page-typo-value">a</button>
-                        <button class="block-card-custom-page-typo-value">A</
-                        >
-                    </div>
+        <div class="block-card block-card-custom-page info">
+            <div class="block-card-custom-page-info-property">Auteur<span class="block-card-custom-page-info-value"></span>
+                <div hidden>
+                    <input type="number" name="id" value="<?php echo $this->data['post']->getId() ?? '' ?>"/>
                 </div>
             </div>
-        </div>
-        <div class="block-card block-card-custom-page info">
-            <div class="block-card-custom-page-info-property">Auteur<span
-                        class="block-card-custom-page-info-value">Yves</span></div>
             <div class="block-card-custom-page-info-property">Statut<span class="block-card-custom-page-info-value">Brouillon</span>
             </div>
-            <div class="block-card-custom-page-info-property">Date<span class="block-card-custom-page-info-value">12/12/2023</span>
+            <div class="block-card-custom-page-info-property">Date<span class="block-card-custom-page-info-value"><?php echo $this->data['post']->getCreatedat() ?? '' ?>"</span>
             </div>
-            <div class="block-card-custom-page-info-property">URL<span class="block-card-custom-page-info-value">gofindme.com/home</span>
-            </div>
-        </div>
-        <div class="block-card block-card-custom-page media">
-            <div class="block-card-custom-page-media-property">Image<span class="block-card-custom-page-media-value">Ajouter un nouveau contenu</span>
+            <div class="block-card-custom-page-info-property">URL<span class="block-card-custom-page-info-value">monsite/<?php echo $this->data['post']->getSlug() ?? '' ?></span>
             </div>
         </div>
         <div class="button-new-page">
             <div class="button-submit">
-                <button class="button button-primary button-lg">Sauvegarder</button>
+                <button class="button button-primary button-lg">Brouillon</button>
             </div>
-            <div class="button-delet">
-                <button class="button button-danger button-lg">Supprimer</button>
+            <div class="button-submit">
+                <button class="button button-secondary button-lg">Publiée
+                    <div hidden>
+                        <input type="number" name="isPublished" value="<?php echo $this->data['post']->getPublished() ?? '0' ?>"/>
+                    </div>
+                </button>
+            </div>
+            <div class="button-deleted">
+                <button class="button button-danger button-lg">Supprimer
+                    <div hidden>
+                        <input type="number" name="isDeleted" value="<?php echo $this->data['post']->getIsDeleted() ?? '0' ?>"/>
+                    </div>
+                </button>
             </div>
         </div>
     </div>
