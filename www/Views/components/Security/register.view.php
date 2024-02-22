bca0ed6ee82d31d9223d274027631d98e09c0244
cgirard <camille.girard1995@gmail.com>
2024-02-22 01:32:12 +0100

    :sparkles: upd

--- www/Views/components/Security/register.view.php
+++ www/Views/components/Security/register.view.php
@@ -1,1 +1,6 @@
-<?php $this->includeComponent("form", $configForm, $errorsForm, $successForm, "button button-primary");
+<section class="connection-box">
+    <div class="box-title"><h3>Inscription</h3></div>
+    <div class="box-content">
+        <?php $this->includeComponent("form", $configForm, $errorsForm, $successForm, "button button-primary")?>
+    </div>
+</section>
